cb7c57267610413dcf4b3a96d732b7dfce5c331e
emilyLK-77 <larimer.emily.l@gmail.com>
2025-02-11 00:32:55 +0000

    add to assignment week 5

---
+++ homework5_Emily_Larimer.py
@@ -0,0 +1,16 @@
+flower = ["rose", "lily", "daisy", "iris", "peony", "tulip"]    
+print(flower)   
+s1flower = flower[0:2]
+print("The first two items in the list are:", s1flower)   
+s2flower = flower [2:4]
+print("The middle two items in the list are:", s2flower)
+print("The first and last items in the list are:", flower[0], flower[5])
+
+menu = ("apple", "banana", "orange", "kiwi", "blueberry")
+for fruit in menu:
+    print(fruit)
+
+print("new menu:")
+menu_2 = ("mango", "guava", "orange", "kiwi", "blueberry")
+for fruit2 in menu_2:
+    print(fruit2)
